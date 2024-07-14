dc4d8a6dfe12474f0e0c8f7f0ecb4f88e6893f06
Imran <imranbinazad777@gmail.com>
2024-07-14 15:42:07 +0600

    authentication and database added in backend

---
+++ backend/src/main/java/com/aicademy/backend/models/Role.java
@@ -0,0 +1,20 @@
+package com.aicademy.backend.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    private ObjectId id;
+
+    private String name;
+}
