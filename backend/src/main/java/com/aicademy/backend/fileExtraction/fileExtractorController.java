95916079db5b32795e1df3356d792c54bfde1682
Imran <imranbinazad777@gmail.com>
2024-09-09 19:22:32 +0600

    ai chatbot and fileExtraction added

---
+++ backend/src/main/java/com/aicademy/backend/fileExtraction/fileExtractorController.java
@@ -0,0 +1,27 @@
+package com.aicademy.backend.fileExtraction;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/public/extractor")
+@RequiredArgsConstructor
+@Validated
+public class fileExtractorController {
+
+    private final ContentExtractorService contentExtractorControl;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(OK)
+    public ResponseEntity<ContentResponseDto> classify(@RequestParam("file") final MultipartFile pdfFile) {
+        return ResponseEntity.ok().body(ContentResponseDto.builder().content(this.contentExtractorControl.extractContent(pdfFile)).build());
+    }
+
+
+}
