02bdea5f9060c0ede59298c49e48781c9707b503
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-09-10 16:42:14 +0600

    test

--- frontend/src/pages/Quizz.jsx
+++ frontend/src/pages/Quizz.jsx
@@ -2,9 +2,7 @@ import React, { useContext } from "react";
 import { useState, useEffect } from "react";
 import quiz from "../assets/quiz.png";
 // import note from "../assets/note.png";
-import { useNavigate } from "react-router-dom";
 
-import { userContextProvider } from "../context/UserContext";
 const Quizz = () => {
     const navigate=useNavigate()
     const { curTopic } = useContext(userContextProvider);
